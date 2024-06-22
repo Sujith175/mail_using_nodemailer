c9c27b3c0c1334782b6c69bf7ac8bf7d775ba0c3
Sujith Kumar R <sujithkumar.sk175@gmail.com>
2024-06-22 13:20:25 +0530

    added

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const express = require("express");
-const dotenv = require("dotenv");
+
 const app = express();
 const mail = require("./Controllers/mail");
 const demo = require("./Controllers/mail");
-
-dotenv.config();
-
+require("dotenv").config;
 app.use("/api/users", mail);
 app.use(demo);
+
 app.listen(process.env.PORT, () => {
   console.log("listening to port 5000");
 });
